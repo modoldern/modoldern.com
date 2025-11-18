54fca973cedd2a2f8daadd20fa006db5bf0bd1b0
modoldern <modoldern@gmail.com>
2025-11-19 01:33:20 +0400

    test

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ let langUI = {};
 const contentEl = document.getElementById("content");
 
 // Boot animation
-setTimeout(() => document.getElementById("boot").style.display = "none", 1500);
+setTimeout(() => document.getElementById("boot").style.display = "none");
 
 // --- THEME ---
 let theme = localStorage.getItem("theme") || "dark";
